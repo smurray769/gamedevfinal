32fd96861cf8e5fd3bd3289871bb10629f598f7b
smurray769 <91291246+smurray769@users.noreply.github.com>
2023-05-01 17:30:30 -0500

    vertical layout group + fixed bug
    added vertical layout group to the pause menu and fixed the bug where clicking pause menu options would cause the character to shoot

--- final project/Assets/Scripts/Combat/Weapon.cs
+++ final project/Assets/Scripts/Combat/Weapon.cs
@@ -16,7 +16,7 @@ public class Weapon : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1")) 
+        if (Input.GetButtonDown("Fire1") && !PauseMenu.GameIsPaused) 
         {
             animator.SetTrigger("isShooting");
             Shoot();
