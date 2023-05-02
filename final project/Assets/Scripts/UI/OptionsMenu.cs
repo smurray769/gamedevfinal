e07c8d94f02d035462498a9df85c039aee9e0da2
smurray769 <91291246+smurray769@users.noreply.github.com>
2023-05-02 12:02:43 -0500

    volume sliders working

--- final project/Assets/Scripts/UI/OptionsMenu.cs
+++ final project/Assets/Scripts/UI/OptionsMenu.cs
@@ -1,12 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class OptionsMenu : MonoBehaviour
 {
     //resolution
     
     //volume
+    public AudioMixer audioMixer;
+
+    public void SetMasterVolume(float newVolume)
+    {
+        audioMixer.SetFloat("MasterVolume", newVolume);
+    }
+
+    public void SetAtmosphereVolume(float newVolume)
+    {
+        audioMixer.SetFloat("AtmosphereVolume", newVolume);
+    }
+
+    public void SetMusicVolume(float newVolume)
+    {
+        audioMixer.SetFloat("MusicVolume", newVolume);
+    }
+
+    public void SetSFXVolume(float newVolume)
+    {
+        audioMixer.SetFloat("SFXVolume", newVolume);
+    }
 
     //back button
     public void GoBack()
